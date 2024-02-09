bf8cef1cdb838d43a4f1e1fda95c9749d675e1b9
Priscilla Emasoga <priscillaemasoga@gmail.com>
2024-02-09 09:21:27 +0000

    Add login and register pages

---
+++ robin/app/login/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export default function Login() {
+    return (
+      <main>
+        <p>Login Page</p>
+      </main>
+    );
+}
+
+export const metadata: Metadata = {
+  title: "Robin App - Login",
+  description: "Login into your account!",
+};
---
+++ robin/app/register/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next"
+
+export default function Register() {
+  return (
+    <main>
+      <p>Register Page</p>
+    </main>
+  )
+}
+
+export const metadata: Metadata = {
+  title: "Robin App - Register",
+  description: "Register for a new account!",
+}
