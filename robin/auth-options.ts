b0b84a24217a925fc6cc6835dac26ea960460350
Priscilla Emasoga <priscillaemasoga@gmail.com>
2024-02-09 19:40:14 +0000

    Add auth options config file

---
+++ robin/auth-options.ts
@@ -0,0 +1,20 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+// import { PrismaAdapter } from "@next-auth/prisma-adapter";
+// import prisma from "@/prisma/client";
+
+const authOptions: NextAuthOptions = {
+  // adapter: PrismaAdapter(prisma),
+  providers: [
+    // OAuth authentication providers...
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+};
+
+export default authOptions;
