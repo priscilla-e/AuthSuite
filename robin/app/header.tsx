42232d3bdb961d6ea8b51f9da0991f25733f653e
Priscilla Emasoga <priscillaemasoga@gmail.com>
2024-02-09 19:43:01 +0000

    Add logout functionality

---
+++ robin/app/header.tsx
@@ -0,0 +1,33 @@
+'use client';
+import Link from "next/link";
+import { useSession, signOut } from "next-auth/react"
+
+export default function Header() {
+  const { data: session } = useSession();
+
+  const handleSignOut = async () => {
+    signOut();
+  }
+
+  return (
+    <header className=" bg-blue-400 text-gray-800 p-4">
+      <div className="container mx-auto flex items-center justify-between">
+        <div className="text-2xl font-bold text">Robin Auth App</div>
+        <nav className="space-x-4">
+          {!session ? (
+            <Link
+              href="/login"
+              className="mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-white"
+            >
+              Login
+            </Link>
+          ) : (
+            <button type="button" className="text-white text-sm bg" onClick={handleSignOut}>
+              Sign Out
+            </button>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
+}
