790b5bd823b29a4fe80cc60114c363ba417a26ba
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2024-06-10 23:13:43 +0100

    Add authApi functions

--- token-extended/client/src/api/auth.ts
+++ token-extended/client/src/api/auth.ts
@@ -1,2 +1,35 @@
-// export async function login(email: string, password: string) {
-// }
+import axiosClient from "../lib/axios-client";
+import { User } from "../types";
+
+
+export async function getCurrentUser(): Promise<User>{
+    const response = await axiosClient.get('/auth/user')
+    return response.data
+}
+
+export async function login(email: string, password: string): Promise<{
+    user: User;
+    access_token: string;
+    msg: string;
+}> {
+    const response = await axiosClient.post('/auth/login', {
+        email,
+        password
+    })
+    return response.data
+}
+
+export async function register(email: string, password: string): Promise<{
+    msg: string;
+}> {
+    const response = await axiosClient.post('/auth/register', {
+        email,
+        password
+    })
+    return response.data
+}
+
+export async function logout(): Promise<{msg: string}> {
+    const response = await axiosClient.post('/auth/logout')
+    return response.data
+}
