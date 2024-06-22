8caebbe29ab9999d4039cd7159573e153d11a00c
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2024-06-22 02:30:31 +0100

    Add AuthProvider to root component for authentication context

--- token-extended/client/src/pages/root.tsx
+++ token-extended/client/src/pages/root.tsx
@@ -1,9 +1,12 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from 'react-router-dom';
+import AuthProvider from '../context/auth/auth-provider';
 
 export default function Root() {
     return (
-        <main>
-            <Outlet />
-        </main>
-    )
+        <AuthProvider>
+            <main>
+                <Outlet />
+            </main>
+        </AuthProvider>
+    );
 }
