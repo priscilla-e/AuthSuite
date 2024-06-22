80d466dd6215297424e67d5bb1ccb466f72749fe
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2024-06-22 02:28:39 +0100

    Complete home page

--- token-extended/client/src/pages/home.tsx
+++ token-extended/client/src/pages/home.tsx
@@ -1,8 +1,17 @@
-import useAuth from "../hooks/useAuth"
+import useAuth from '../hooks/useAuth';
 export default function Home() {
-    const { user } = useAuth();
+    const { user, logout } = useAuth();
 
     return (
-        <h1 className="text-4xl">Welcome to the homepage {`${user}`}!</h1>
-    )
+        <div className="min-h-screen container mx-auto flex space-x-4 items-center justify-center">
+            <div className="card w-1/3 bg-base-100 shadow-xl px-16 py-20">
+                <h1 className="text-4xl">
+                    Welcome to the homepage {`${user?.email}`}!
+                </h1>
+                <button className="btn btn-primary" onClick={logout}>
+                    Logout
+                </button>
+            </div>
+        </div>
+    );
 }
