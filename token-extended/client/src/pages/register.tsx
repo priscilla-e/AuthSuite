40288957fda365d39d01f3b3e2d385822260e5cd
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2024-06-22 02:28:22 +0100

    Complete register page

--- token-extended/client/src/pages/register.tsx
+++ token-extended/client/src/pages/register.tsx
@@ -1,5 +1,71 @@
+import { useState } from "react";
+import useAuth from "../hooks/useAuth";
+import { Link } from "react-router-dom";
+
 export default function Register() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const { register } = useAuth();
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        register(email, password);
+    }
+
     return (
-        <p>Register page</p>
-    )
+        <div className="min-h-svh mx-auto container flex justify-center items-start mt-20">
+            <div className="card w-1/3 bg-base-100 shadow-xl px-16 py-20">
+                <h1 className="text-4xl mb-10"> Create a new account</h1>
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <label className="input input-bordered flex items-center gap-2">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 16 16"
+                            fill="currentColor"
+                            className="w-4 h-4 opacity-70"
+                        >
+                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                        </svg>
+                        <input
+                            value={email}
+                            type="text"
+                            className="grow"
+                            placeholder="Email"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </label>
+                    <label className="input input-bordered flex items-center gap-2">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 16 16"
+                            fill="currentColor"
+                            className="w-4 h-4 opacity-70"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                        <input
+                            value={password}
+                            type="password"
+                            className="grow"
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Password"
+                        />
+                    </label>
+
+                    <button type="submit" className="btn btn-primary">
+                        Register
+                    </button>
+                </form>
+                <div className="mt-10">
+                    <span> Already registered? </span>
+                    <Link to="/login" className="text-primary">Login </Link>
+                </div>
+            </div>
+        </div>
+    );
 }
